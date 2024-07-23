5f9f916576e7e7d51a304fcf6bbbb8d44c4eb319
betinkaraman <132080205+betinkaraman@users.noreply.github.com>
2024-07-23 19:43:33 +0300

    Add files via upload

---
+++ embassy.py
@@ -0,0 +1,6 @@
+# Embassy List
+Embassies = {
+    # [EMBASSY (COUNTRY CODE), FACILITY_ID (EMBASSY ID), "Continue in different languages"],
+    "en-tr-ankara": ["en-tr", 124, "Continue"], # English - Turkey - Ankara
+    "en-tr-istanbul": ["en-tr", 125, "Continue"], # English - Turkey - Istanbul
+}
